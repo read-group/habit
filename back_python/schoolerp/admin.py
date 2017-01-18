9a2a15e99871979a040adfded708f6413db26d46
jiangyong <jiangyong@qq.com>
2017-01-18 23:32:11 +0800

    dd

--- back_python/schoolerp/admin.py
+++ back_python/schoolerp/admin.py
@@ -2,4 +2,6 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import School
+from .models import ClassGroup
 admin.site.register(School)
+admin.site.register(ClassGroup)
