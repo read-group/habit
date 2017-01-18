f55d313ce12df75b1db2b2cbd01f1d5dbe54d2b9
jiangyong <jiangyong@qq.com>
2017-01-18 23:49:20 +0800

    dd

--- back_python/schoolerp/models.py
+++ back_python/schoolerp/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class EntityBase(models.Model):
-    code =models.CharField(max_length=30)
-    name = models.CharField(max_length=100)
+    code =models.CharField(max_length=30,verbose_name="编码")
+    name = models.CharField(max_length=100,verbose_name="名称")
     class Meta:
         abstract=True
 
