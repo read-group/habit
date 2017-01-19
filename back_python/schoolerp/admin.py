e316cc96ab2b6ca90454246f9456970699d1e694
jiangyong <jiangyong@qq.com>
2017-01-19 22:35:34 +0800

    test

--- back_python/schoolerp/admin.py
+++ back_python/schoolerp/admin.py
@@ -3,5 +3,15 @@ from django.contrib import admin
 # Register your models here.
 from .models import School
 from .models import ClassGroup
-admin.site.register(School)
+
+
+
+class ClassInline(admin.TabularInline):
+    model = ClassGroup
+    extra = 3
+class SchoolAdmin(admin.ModelAdmin):
+    list_display=('code','name')
+    inlines = [ClassInline]
+
+admin.site.register(School,SchoolAdmin)
 admin.site.register(ClassGroup)
--- back_python/schoolerp/tests.py
+++ back_python/schoolerp/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
 # Create your tests here.
+import datetime
+
+from django.utils import timezone
+from .models import School
+
+
+class QuestionMethodTests(TestCase):
+
+    def test_q(self):
+        """
+        query count test
+        """
+        for item in School.objects.all():
+            print(item)
+        #self.assertIs(School.objects.count()==1, True)
