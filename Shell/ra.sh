e4a4a7d354442ab4169a9903b3e1ce5ce44aea1a
jiangyong <jiangyong@qq.com>
2017-02-18 01:28:52 +0800

    dd

--- Shell/ra.sh
+++ Shell/ra.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-#杀死node进程
+#杀死进程
 netstat -tnlup | grep uwsgi | awk '{print $7}' | cut -d '/' -f1 | xargs   kill -9
-
+netstat -tnlup | grep node | awk '{print $7}' | cut -d '/' -f1 | xargs   kill -9
 #启动uwsgi
-uwsgi --ini /mnt/s/mygit/habit/back_python/uwsgi.ini
+/mnt/s/penv/bin/uwsgi --ini /mnt/s/mygit/habit/back_python/uwsgi.ini &
+
+＃启动node
+nohup node /mnt/s/mygit/habit/WxAppServer/wxApp.js & >/dev/null
 #删除会话文件
 /etc/init.d/redis-server stop
 #rm -rf /var/lib/redis/dump.rdb
