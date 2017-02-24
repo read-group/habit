50e70c50fc1b27b532bde3d3db0cbec3ed95aa0d
jiangyong <jiangyong@qq.com>
2017-02-24 18:08:49 +0800

    d

--- back_python/front/views/main.py
+++ back_python/front/views/main.py
@@ -8,29 +8,25 @@ import json
 
 class MainView(TemplateView):
     template_name="front/main.html"
-    def get(self,request,*args,**kwargs):
-        # 获取openid，昵称，头像url,性别等信息
-        # 按照openid,去查询profile,如果没有存在，就创建一个user,同时创建一个profile
-        # 然后去模拟登录login
-        if(request.user.is_authenticated is False):
-            role=request.Get["role"];
-            code=request.Get["code"];
-            wxinfoUrl=settings.WX["WX_AUTH_URL_CODE"].replace("{code}",code);
-            import httplib
-            conn = httplib.HTTPConnection(wxinfoUrl)
-            conn.request("GET", "/index.html")
-            r1 = conn.getresponse()
-            print(r1.status, r1.reason)
-            data1 = r1.read()
-            decodeJson=json.loads(data1)
-            print(decodeJson)
-            conn.close()
-            pass
-        # 去微信认证
-        # 认证通过后，需要创建用户，并login
-        # 重新跳转到本页面
-
-
-
-
-        return super(MainView,self).get(req,args,kwargs)
+    # def get(self,request,*args,**kwargs):
+    #     # 获取openid，昵称，头像url,性别等信息
+    #     # 按照openid,去查询profile,如果没有存在，就创建一个user,同时创建一个profile
+    #     # 然后去模拟登录login
+    #     # if(request.user.is_authenticated is False):
+    #     #     role=request.Get["role"];
+    #     #     code=request.Get["code"];
+    #     #     wxinfoUrl=settings.WX["WX_AUTH_URL_CODE"].replace("{code}",code);
+    #     #     import httplib
+    #     #     conn = httplib.HTTPConnection(wxinfoUrl)
+    #     #     conn.request("GET", "/index.html")
+    #     #     r1 = conn.getresponse()
+    #     #     print(r1.status, r1.reason)
+    #     #     data1 = r1.read()
+    #     #     decodeJson=json.loads(data1)
+    #     #     print(decodeJson)
+    #     #     conn.close()
+    #     #     pass
+    #     # 去微信认证
+    #     # 认证通过后，需要创建用户，并login
+    #     # 重新跳转到本页面
+    #     return super(MainView,self).get(request,*args,**kwargs)
