df1ceec46036de947ebd1bc47c393eb9d46a584e
jiangyong <jiangyong@qq.com>
2017-02-24 21:01:58 +0800

    d

--- back_python/front/views/home.py
+++ back_python/front/views/home.py
@@ -22,10 +22,9 @@ class HomeMobileView(TemplateView):
     template_name="front/mobile.html"
     def get_context_data(self, **kwargs):
         ctx=super(HomeMobileView,self).get_context_data(**kwargs)
-        hostRedirect=settings.WX['WX_APP_REDIRECT'].replace("{role}","host")
-        teacherRedirect=settings.WX['WX_APP_REDIRECT'].replace("{role}","teacher")
-        childRedirect=settings.WX['WX_APP_REDIRECT'].replace("{role}","child")
-
+        hostRedirect=settings.WX['WX_APP_REDIRECT']
+        teacherRedirect=settings.WX['WX_APP_REDIRECT']
+        childRedirect=settings.WX['WX_APP_REDIRECT']
 
         ctx["host"]=settings.WX['WX_AUTH_URL_CODE'].replace("{redirect_uri}",urllib.parse.urlencode({'redirect_uri':hostRedirect}))
         ctx["teacher"]=settings.WX['WX_AUTH_URL_CODE'].replace("{redirect_uri}",urllib.parse.urlencode({'redirect_uri':teacherRedirect}))
