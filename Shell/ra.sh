e1f23b22ff69938ee2f9d1527c99796075a33602
jiangyong <jiangyong@qq.com>
2017-02-25 15:34:25 +0800

    d

--- Shell/ra.sh
+++ Shell/ra.sh
@@ -5,7 +5,7 @@ netstat -tnlup | grep node | awk '{print $7}' | cut -d '/' -f1 | xargs   kill -9
 #启动uwsgi
 /mnt/s/penv/bin/uwsgi --ini /mnt/s/mygit/habit/back_python/uwsgi.ini &
 
-＃启动node
+#启动node
 nohup node /mnt/s/mygit/habit/WxAppServer/wxApp.js & >/dev/null
 #删除会话文件
 /etc/init.d/redis-server stop
