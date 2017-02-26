5d9c1f1d9d79e121de492c3a95b5ae36faa2bfec
jiangyong <jiangyong@qq.com>
2017-02-26 10:27:29 +0800

    d

--- back_python/front/static/front/js/mily/yml.sugar.js
+++ back_python/front/static/front/js/mily/yml.sugar.js
@@ -28,7 +28,7 @@
 				 });
 			});
 			this.soundsArray=[];
-			this.sound=new buzz.sound("/static/front/buzz/sounds/btnClick.mp3");
+			this.sound=new buzz.sound("/static/front/js/buzz/sounds/btnClick.mp3");
 			this.isPlay=false;
 		},
 		openSound:function(){
