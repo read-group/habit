84ef0ce93daa93ddc7cacac88ba51980b83b7b39
jiangyong <jiangyong@qq.com>
2017-03-05 16:33:03 +0800

    d

--- back_python/api/views/activity.py
+++ back_python/api/views/activity.py
@@ -16,7 +16,7 @@ class ActivityView(JsonResultView):
             limit=skip+reqData["pageParam"]["limit"]
             acts= Activity.objects.order_by("-createdTime")[skip:limit]
             for act in acts:
-                dataTmp=self.toJSON(act,["name","code","startTime","endTime","desc"])
+                dataTmp=self.toJSON(act,["id","name","code","startTime","endTime","desc"])
                 dataTmp["img"]=req.scheme+"://"+req.META["HTTP_HOST"]+settings.MEDIA_URL+act.img.img.name
                 dataTmp["cat"]=act.get_cat_display()
                 data.append(dataTmp)
