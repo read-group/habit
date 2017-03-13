4052f02989faff6f882e09968d03b6feb4e29afb
jiangyong <jiangyong@qq.com>
2017-03-13 22:54:00 +0800

    d

--- back_python/activity/admin.py
+++ back_python/activity/admin.py
@@ -9,7 +9,7 @@ class ActivityItemInline(admin.TabularInline):
     fields=('cat',)
 class ActivityAdmin(admin.ModelAdmin):
     change_form_template = 'activity/change_form.html'
-    fields=('code','name',('startTime','endTime',),('cat','desc'),'img','amount',('status','isTop',),'memo')
+    fields=('code','name',('startTime','endTime',),('cat','desc'),'img','amount','zeroableMily',('status','isTop',),'memo')
     list_display=('code','name','startTime','endTime','status')
     inlines = [ActivityItemInline]
     list_filter=('cat','status')
