cdeed3f72182cc5502e75fd3978583bfcb506d3a
jiangyong <jiangyong@qq.com>
2017-03-19 12:02:04 +0800

    d

--- back_python/api/service/activityService.py
+++ back_python/api/service/activityService.py
@@ -58,12 +58,12 @@ class ActivityService(JsonResultService):
             orgActivityKey=settings.CACHE_FORMAT_STR['org_activity'] % (org.id)
             orgActivityHistorys=cache.get(orgActivityKey)
             dataTmp["applied"]=False
-            if not orgActivityHistorys:
-                pass
-            else:
-                for oac in orgActivityHistorys:
-                    if oac.activity.id==dataTmp.id:
-                        dataTmp["applied"]=True
+            # if not orgActivityHistorys:
+            #     pass
+            # else:
+            #     for oac in orgActivityHistorys:
+            #         if oac.activity.id==dataTmp.id:
+            #             dataTmp["applied"]=True
             content["data"]=dataTmp
         except:
             info=sys.exc_info()
