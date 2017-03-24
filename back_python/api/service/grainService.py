4490344c5eb27e4db861d73a4ee238da1477ae81
jiangyong <jiangyong@qq.com>
2017-03-25 07:18:57 +0800

    d

--- back_python/api/service/grainService.py
+++ back_python/api/service/grainService.py
@@ -1,5 +1,6 @@
 from django.db import transaction
 from django.db.models import Q
+from org.models import Profile
 import datetime
 from activity.models import Activity
 from django.conf import settings
@@ -12,22 +13,19 @@ import json
 from django.core.cache import cache
 import logging
 logger = logging.getLogger("django")
-from org.models import Profile
+
 # Create your views here.
 class GrainService(JsonResultService):
     def family(self,familyOrg):
         content={}
         data=[]
         logger.error("GrainService")
-        logger.error(familyOrg.id)
         try:
             profiles=Profile.objects.filter(org__id__exact=familyOrg.id).order_by("createdTime");
-            logger.error(profiles)
             for profile in profiles:
                 dataTmp=self.toJSON(profile,["id","nickname","imgUrl","role","openid"])
                 data.append(dataTmp)
             content["data"]=data
-            logger.error(data)
         except:
             info=sys.exc_info()
             logging.error(info)
