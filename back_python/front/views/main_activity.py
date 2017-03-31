375955d204729ac01ed29ecaf4b4bbf91cc8813c
jiangyong <jiangyong@qq.com>
2017-03-31 17:17:50 +0800

    d

--- back_python/front/views/main_activity.py
+++ back_python/front/views/main_activity.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.conf import settings
 import json
-import urllib.request
+import urllib
 # Create your views here.
 # def index(request):
 #     return render(request,"front/index.html");
