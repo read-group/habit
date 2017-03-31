575850b68d534a07d7f95365b8370521c85a0ece
jiangyong <jiangyong@qq.com>
2017-03-31 18:01:19 +0800

    d

--- back_python/front/views/main_activity.py
+++ back_python/front/views/main_activity.py
@@ -21,8 +21,8 @@ class ActivityDetailView(TemplateView):
         path=self.request.path
         hostRedirect=settings.WX['WX_APP_REDIRECT'].replace("{role}","host")+"&pathfrom="+path
         encode=urllib.parse.urlencode({'redirect_uri':hostRedirect})
-        logger.error(encode)
-        ctx["host"]=settings.WX['WX_AUTH_URL_CODE'].replace("{redirect_uri}",encode)
+        ctx["detailUrl"]=settings.WX['WX_AUTH_URL_CODE'].replace("{redirect_uri}",encode)
+        logger.error(ctx["detailUrl"])
         return ctx
     def get(self,request,*args,**kwargs):
         return super(ActivityDetailView,self).get(request,*args,**kwargs)
