8c623edbf6f95ee6aff3559a2c3be2d791444883
jiangyong <jiangyong@qq.com>
2017-03-31 17:37:25 +0800

    d

--- back_python/front/views/main_activity.py
+++ back_python/front/views/main_activity.py
@@ -19,7 +19,7 @@ class ActivityDetailView(TemplateView):
         ctx=super(ActivityDetailView,self).get_context_data(**kwargs)
         # ctx["refererUrl"]=self.request.META['HTTP_REFERER']
         path=self.request.path
-        hostRedirect=settings.WX['WX_APP_REDIRECT'].replace("{role}","host")+path
+        hostRedirect=settings.WX['WX_APP_REDIRECT'].replace("{role}","host")+"&pathfrom="+path
         encode=urllib.parse.urlencode({'redirect_uri':hostRedirect})
         logger.error(encode)
         ctx["host"]=settings.WX['WX_AUTH_URL_CODE'].replace("{redirect_uri}",encode)
