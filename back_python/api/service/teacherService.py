c95e40c00a812e13ff4fd2765f603c65b44fa945
jiangyong <jiangyong@qq.com>
2017-04-03 21:44:03 +0800

    d

--- back_python/api/service/teacherService.py
+++ back_python/api/service/teacherService.py
@@ -35,7 +35,7 @@ class TeacherService(JsonResultService):
             profilesRtn=None
             if -1==cgidParam:
                 logger.error("-1============")
-                cg=cgs[0:1]
+                cg=cgs[0]
                 logger.error(cg.name)
                 profilesRtn=cgs[0:1].profile_set.all()
                 logger.error(len(profilesRtn))
