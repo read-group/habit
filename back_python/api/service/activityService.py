bb5a0958e373ce148679e805d31b0afc0f6593d5
jiangyong <jiangyong@qq.com>
2017-04-04 22:01:57 +0800

    d

--- back_python/api/service/activityService.py
+++ back_python/api/service/activityService.py
@@ -182,13 +182,14 @@ class ActivityService(JsonResultService):
                 orgActivityHistory.save()
 
                 # 更新活动报名人数
-                activity.applyNumber=F('applyNumber') + 1
-                # activity.applyNumber=activity.applyNumber+1
+                # activity.applyNumber=F('applyNumber') + 1
+                activity.applyNumber=activity.applyNumber+1
                 if activity.applyNumber>activity.uplimit:
                     self.jsonResult.rtnDic['errMsg']="名额已满，请下期再报！"
                     raise Exception("名额已满，请下期再报！")
                 else:
                     activity.save()
+
 
 
                 # # 平台米仓修改
