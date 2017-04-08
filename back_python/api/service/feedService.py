766f96e5377a655bc50e9c9bf0ae70ffb65e4eff
jiangyong <jiangyong@qq.com>
2017-04-08 16:41:40 +0800

    d

--- back_python/api/service/feedService.py
+++ back_python/api/service/feedService.py
@@ -40,7 +40,7 @@ class FeedbackService(JsonResultService):
                         habittmp["name"]=habitstrArray[1]
                         habittmp["isForParent"]=habitstrArray[2]
                         habittmp["icon"]=habitstrArray[3]
-                        habittmp["isFeedBack"]=False
+                        habittmp["isFeedBack"]="0"
                         if fstr==habittmp["isForParent"]:
                             data.append(habittmp)
                 content["data"]=data
