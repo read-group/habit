815610b3694aa0505bc252ee1296e4e4677a48f5
jiangyong <jiangyong@qq.com>
2017-04-09 19:09:24 +0800

    d

--- back_python/api/service/feedService.py
+++ back_python/api/service/feedService.py
@@ -94,7 +94,6 @@ class FeedbackService(JsonResultService):
                 post.save()
                 # 初始化缓存　Key:打卡用户id+habitid+打卡日期 value:1-表示已经打卡，０表示未打卡
                 logger.error("date format...........")
-                logger.error(dtmpstr)
                 userid_habitid_date_key=settings.CACHE_FORMAT_STR['userid_habitid_date_key'] % (int(pid),int(habitid),post.postDate)
                 cache.set(userid_habitid_date_key,1,settings.CACHE_FORMAT_STR['userid_habitid_date_key_timeout'])
                 # 返回当前帖子
