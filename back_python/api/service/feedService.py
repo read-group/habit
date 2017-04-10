63e004a8fb60e6fea7e27ce770dd562cbebf26d7
jiangyong <jiangyong@qq.com>
2017-04-11 00:55:12 +0800

    d

--- back_python/api/service/feedService.py
+++ back_python/api/service/feedService.py
@@ -169,7 +169,6 @@ class FeedbackService(JsonResultService):
                 accountHistory.feedback=feedBack
                 accountHistory.tradeAmount=feedBack.freeMily
                 accountHistory.save()
-                # 设置当前用户的最新个人账户缓存
                 cache.set(accountkey,accountHistory.account))
                 # 返回当前帖子
                 content["postid"]=post.id
