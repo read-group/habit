29d3454dc96a20ae4c4d5abcbd60f44b07f22344
jiangyong <jiangyong@qq.com>
2017-04-13 22:53:08 +0800

    d

--- back_python/api/service/__init__.py
+++ back_python/api/service/__init__.py
@@ -3,3 +3,4 @@ from .grainService import grainService
 from .feedService import feedbackService
 from .teacherService import teacherService
 from .wxService import wxService
+from .stageService import stageService
