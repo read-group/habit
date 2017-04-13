1bae9a1a0b3fe671a7763450627253dd762fc2f2
jiangyong <jiangyong@qq.com>
2017-04-13 23:25:02 +0800

    d

--- back_python/api/service/stageService.py
+++ back_python/api/service/stageService.py
@@ -30,7 +30,7 @@ class StageService(JsonResultService):
             count=queryCache.count();
             posts= queryCache[skip:limit]
             for post in posts:
-                dataTmp=self.toJSON(post,["id","imgUrl","audioUrls","postDate"])
+                dataTmp=self.toJSON(post,["id","content","imgUrl","audioUrls","postDate"])
                 dataTmp["nickname"]=post.feedBack.profile.nickname
                 dataTmp["headingImgUrl"]=post.feedBack.profile.imgUrl
                 dataTmp["accumDays"]=post.feedBack.accumDays
