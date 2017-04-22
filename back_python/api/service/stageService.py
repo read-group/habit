2b83e64852c3f752ba90a7ce445284279195947f
jiangyong <jiangyong@qq.com>
2017-04-22 12:32:05 +0800

    d

--- back_python/api/service/stageService.py
+++ back_python/api/service/stageService.py
@@ -29,10 +29,8 @@ class StageService(JsonResultService):
             queryCache=None
             if int(pid)==-1:
                 queryCache=Post.objects.select_related('feedBack').order_by("-createdTime");
-                if int(pid)==-2:
-                    queryCache=Post.objects.select_related('feedBack').filter(feedBack__profile__org__id=currentUser.org.id).order_by("-createdTime");
-                else:
-                    queryCache=Post.objects.select_related('feedBack').filter(feedBack__profile__id=int(pid)).order_by("-createdTime");
+            else:
+                queryCache=Post.objects.select_related('feedBack').filter(feedBack__profile__id=int(pid)).order_by("-createdTime");
             count=queryCache.count();
             posts= queryCache[skip:limit]
             for post in posts:
