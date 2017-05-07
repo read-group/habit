19eb8a5365ef7fba469bed1513062fa3a917eb6b
jiangyong <jiangyong@qq.com>
2017-05-07 11:34:05 +0800

    d

--- back_python/front/static/front/js/mily/yml.sugar.js
+++ back_python/front/static/front/js/mily/yml.sugar.js
@@ -493,7 +493,7 @@
 															top += parseInt($button.outerHeight() * 3/4);
 													$popup.css({ left: left + 'px',
 																			 top: top + 'px',
-																			 height:180+'px'
+																			 height:170+'px'
 																		 });
 													// prevent applying position
 													return false;
