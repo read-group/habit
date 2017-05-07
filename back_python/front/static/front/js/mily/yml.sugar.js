b89c04e7d6eb56db0dfeb9b514bd42ef324263f3
jiangyong <jiangyong@qq.com>
2017-05-07 11:18:14 +0800

    d

--- back_python/front/static/front/js/mily/yml.sugar.js
+++ back_python/front/static/front/js/mily/yml.sugar.js
@@ -425,22 +425,22 @@
 									image: '\uf118', // <img src="path/to/image.png" width="16" height="16" alt="" />
 									popup: function( $popup, $button ) {
 													var list_smilies = [
-																	'<img src="/static/front/js/editor/smiley/1.jpg" width="24" height="24" alt="" />',
-																	'<img src="/static/front/js/editor/smiley/2.jpg" width="24" height="24" alt="" />',
-																	'<img src="/static/front/js/editor/smiley/3.jpg" width="24" height="24" alt="" />',
-																	'<img src="/static/front/js/editor/smiley/4.jpg" width="24" height="24" alt="" />',
-																	'<img src="/static/front/js/editor/smiley/5.jpg" width="24" height="24" alt="" />',
-																	'<img src="/static/front/js/editor/smiley/6.jpg" width="24" height="24" alt="" />',
-																	'<img src="/static/front/js/editor/smiley/7.jpg" width="24" height="24" alt="" />',
-																	'<img src="/static/front/js/editor/smiley/8.jpg" width="24" height="24" alt="" />',
-																	'<img src="/static/front/js/editor/smiley/9.jpg" width="24" height="24" alt="" />',
-																	'<img src="/static/front/js/editor/smiley/10.jpg" width="24" height="24" alt="" />',
-																	'<img src="/static/front/js/editor/smiley/11.jpg" width="24" height="24" alt="" />',
-																	'<img src="/static/front/js/editor/smiley/12.jpg" width="24" height="24" alt="" />',
-																	'<img src="/static/front/js/editor/smiley/13.jpg" width="24" height="24" alt="" />',
-																	'<img src="/static/front/js/editor/smiley/14.jpg" width="24" height="24" alt="" />',
-																	'<img src="/static/front/js/editor/smiley/15.jpg" width="24" height="24" alt="" />',
-																	'<img src="/static/front/js/editor/smiley/16.jpg" width="24" height="24" alt="" />',
+																	'<img src="/static/front/js/editor/smiley/1.png" width="24" height="24" alt="" />',
+																	'<img src="/static/front/js/editor/smiley/2.png" width="24" height="24" alt="" />',
+																	'<img src="/static/front/js/editor/smiley/3.png" width="24" height="24" alt="" />',
+																	'<img src="/static/front/js/editor/smiley/4.png" width="24" height="24" alt="" />',
+																	'<img src="/static/front/js/editor/smiley/5.png" width="24" height="24" alt="" />',
+																	'<img src="/static/front/js/editor/smiley/6.png" width="24" height="24" alt="" />',
+																	'<img src="/static/front/js/editor/smiley/7.png" width="24" height="24" alt="" />',
+																	'<img src="/static/front/js/editor/smiley/8.png" width="24" height="24" alt="" />',
+																	'<img src="/static/front/js/editor/smiley/9.png" width="24" height="24" alt="" />',
+																	'<img src="/static/front/js/editor/smiley/10.png" width="24" height="24" alt="" />',
+																	'<img src="/static/front/js/editor/smiley/11.png" width="24" height="24" alt="" />',
+																	'<img src="/static/front/js/editor/smiley/12.png" width="24" height="24" alt="" />',
+																	'<img src="/static/front/js/editor/smiley/13.png" width="24" height="24" alt="" />',
+																	'<img src="/static/front/js/editor/smiley/14.png" width="24" height="24" alt="" />',
+																	'<img src="/static/front/js/editor/smiley/15.png" width="24" height="24" alt="" />',
+																	'<img src="/static/front/js/editor/smiley/16.png" width="24" height="24" alt="" />',
 													];
 													var $smilies = $('<div/>').addClass('well')
 																										.attr('unselectable','on');
