9c3f6648fa570ef962f6bd5567263a1eecb95adf
jiangyong <jiangyong@qq.com>
2017-05-07 10:22:17 +0800

    d

--- back_python/front/static/front/js/mily/yml.sugar.js
+++ back_python/front/static/front/js/mily/yml.sugar.js
@@ -354,7 +354,7 @@
             type: "post",//使用get方法访问后台
             dataType: "json",//返回json格式的数据
             url: url,//要访问的后台地址
-            data: paramObj,//要发送的数据
+            data: JSON.stringify(paramObj),//要发送的数据
             //complete :function(){$("#load").hide();},//AJAX请求完成时隐藏loading提示
             success: function(res){//msg为返回的数据，在这里做数据绑定
 						  	var obj=JSON.parse(res);
