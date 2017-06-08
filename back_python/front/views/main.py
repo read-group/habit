76dfa4ef770815ae2f055bb0482970683ab3d28e
jiangyong <jiangyong@qq.com>
2017-06-09 06:18:38 +0800

    d

--- back_python/front/views/main.py
+++ back_python/front/views/main.py
@@ -48,37 +48,44 @@ class MainView(TemplateView):
             logger.error("not logined=================================")
             fp1=None
             fp2=None
+            openid=None
+            decodeUserInfoJson=None
             try:
                 role=request.GET["role"];
-                # 检查
-                if role=="child":
-                    # 重定向到学生登录页面,这个url
-                     return HttpResponseRedirect("/stlogin")
+                # 存在openid说明已经注册过了，无需再去注册，直接验证登录
+                if "oid" in request.GET:
+                    openid=request.GET["oid"]
+                else:
+                    # 检查
+                    if role=="child":
+                        # 重定向到学生登录页面,这个url
+                         return HttpResponseRedirect("/stlogin")
 
 
-                code=request.GET["code"];
-                wxinfoUrl=settings.WX["WX_AUTH_URL_INFO"].replace("{code}",code);
-                # logger.debug(wxinfoUrl)
-                import urllib.request
-                fp1 = urllib.request.urlopen(wxinfoUrl)
-                r1 = fp1.read()
-                # logger.debug("data1================")
-                # logger.debug(r1)
-                decodeJson=json.loads(r1)
-                # logger.debug(decodeJson["access_token"])
-                # logger.debug(decodeJson["openid"])
-                # 按照访问token再去获取用户信息
-                userInfoUrl=settings.WX["WX_AUTH_USER_INFO"] % (decodeJson["access_token"],decodeJson["openid"])
-                fp2=urllib.request.urlopen(userInfoUrl)
-                r2 = fp2.read()
-                decodeUserInfoJson=json.loads(r2)
-                fp1.close()
-                fp2.close()
+                    code=request.GET["code"];
+                    wxinfoUrl=settings.WX["WX_AUTH_URL_INFO"].replace("{code}",code);
+                    # logger.debug(wxinfoUrl)
+                    import urllib.request
+                    fp1 = urllib.request.urlopen(wxinfoUrl)
+                    r1 = fp1.read()
+                    # logger.debug("data1================")
+                    # logger.debug(r1)
+                    decodeJson=json.loads(r1)
+                    # logger.debug(decodeJson["access_token"])
+                    # logger.debug(decodeJson["openid"])
+                    # 按照访问token再去获取用户信息
+                    userInfoUrl=settings.WX["WX_AUTH_USER_INFO"] % (decodeJson["access_token"],decodeJson["openid"])
+                    fp2=urllib.request.urlopen(userInfoUrl)
+                    r2 = fp2.read()
+                    decodeUserInfoJson=json.loads(r2)
+                    fp1.close()
+                    fp2.close()
+                    openid=decodeUserInfoJson["openid"]
 
                 # 按照openid查找用户，如果不存在就创建，存在就绕过
                 userTry=None
                 try:
-                    userTry= User.objects.get(username__exact=decodeJson["openid"])
+                    userTry= User.objects.get(username__exact=openid)
                     profileTmp=userTry.profile
                     if role=="teacher" and profileTmp.role=="1":
                         profileTmp.role="3"
@@ -86,14 +93,14 @@ class MainView(TemplateView):
                 except User.DoesNotExist:
                     if role!="child":
                         # 创建新的用户信息
-                        userTry=User.objects.create(username=decodeUserInfoJson["openid"])
+                        userTry=User.objects.create(username=openid)
                         #创建另一个Profile
                         # 如果role=家长，就创建一个家庭
                         orgC=None
                         if role=="host":
-                            orgC=Org.objects.create(code=decodeUserInfoJson["openid"],name=decodeUserInfoJson["nickname"])
+                            orgC=Org.objects.create(code=openid,name=decodeUserInfoJson["nickname"])
                         profile=Profile(nickname=decodeUserInfoJson["nickname"],
-                        openid=decodeUserInfoJson["openid"],role=MapEngToRole[role],
+                        openid=openid,role=MapEngToRole[role],
                         imgUrl=decodeUserInfoJson["headimgurl"],user=userTry,org=orgC)
                         profile.save()
                         #创建三个个人账户（米仓、现金、押金）
@@ -109,12 +116,12 @@ class MainView(TemplateView):
                         # 默认创建一个默认孩子的用户
                         # nkname=decodeUserInfoJson["nickname"]+"@子"
                         nkname="修改昵称"
-                        userC=User.objects.create(username=decodeUserInfoJson["openid"]+"@")
+                        userC=User.objects.create(username=openid+"@")
                         # 查询班级
                         classGroupF=None
                         #创建另一个Profile
                         profileC=Profile(nickname=nkname,role=MapEngToRole["child"],
-                        imgUrl=decodeUserInfoJson["headimgurl"],user=userC,org=orgC,childpwd="987654",openid=decodeUserInfoJson["openid"])
+                        imgUrl=decodeUserInfoJson["headimgurl"],user=userC,org=orgC,childpwd="987654",openid=openid)
                         profileC.save()
 
                         #创建三个个人账户（米仓、现金、押金）
