8872c8b4514d98f2170e5b66f108b718f5addfc3
jiangyong <jiangyong@qq.com>
2017-08-19 22:01:32 +0800

    d

--- back_python/front/views/home.py
+++ back_python/front/views/home.py
@@ -23,12 +23,13 @@ class HomeMobileView(TemplateView):
     def get_context_data(self, **kwargs):
         ctx=super(HomeMobileView,self).get_context_data(**kwargs)
         hostRedirect=settings.WX['WX_APP_REDIRECT'].replace("{role}","host")
+        state=""
         if  "pathfrom" in self.request.GET:
-            hostRedirect=hostRedirect+"&pathfrom="+self.request.GET["pathfrom"]
+            state=hostRedirect+"&pathfrom="+self.request.GET["pathfrom"]
         teacherRedirect=settings.WX['WX_APP_REDIRECT'].replace("{role}","teacher")
         childRedirect=settings.WX['WX_APP_REDIRECT'].replace("{role}","child")
 
-        ctx["host"]=settings.WX['WX_AUTH_URL_CODE'].replace("{redirect_uri}",urllib.parse.urlencode({'redirect_uri':hostRedirect})).replace("{state}","")
+        ctx["host"]=settings.WX['WX_AUTH_URL_CODE'].replace("{redirect_uri}",urllib.parse.urlencode({'redirect_uri':hostRedirect})).replace("{state}",state)
         ctx["teacher"]=settings.WX['WX_AUTH_URL_CODE'].replace("{redirect_uri}",urllib.parse.urlencode({'redirect_uri':teacherRedirect}))
         ctx["child"]=settings.WX['WX_AUTH_URL_CODE'].replace("{redirect_uri}",urllib.parse.urlencode({'redirect_uri':childRedirect}))
         return ctx
